27cfef70c658919db7e84dd55949c20aabc427f4
dmitry <gasanyandmitry@yandex.ru>
2023-09-02 22:38:02 +0300

    add telegram client

---
+++ clients/telegram/telegram.go
@@ -0,0 +1,106 @@
+package telegram
+
+import (
+	"encoding/json"
+	"github.com/dmitry1721/adviser-bot.git/lib/e"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+)
+
+type Client struct {
+	host     string
+	basePath string
+	client   http.Client
+}
+
+const (
+	getUpdatesMethod  = "getUpdates"
+	sendMessageMethod = "sendMessage"
+)
+
+func New(host, token string) Client {
+	return Client{
+		host:     host,
+		basePath: newBasePath(token),
+		client:   http.Client{},
+	}
+}
+
+func newBasePath(token string) string {
+	return "bot" + token
+}
+
+func (c *Client) Updates(offset int, limit int) (updates []Update, err error) {
+	defer func() {
+		err = e.WrapIfErr("can't do request", err)
+	}()
+
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+
+	data, err := c.doRequest(getUpdatesMethod, q)
+	if err != nil {
+		return nil, err
+	}
+
+	var res UpdatesResponse
+
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+
+	return res.Result, nil
+}
+
+func (c *Client) SendMessage(chatId int, text string) error {
+	q := url.Values{}
+	q.Add("chat_id", strconv.Itoa(chatId))
+	q.Add("text", text)
+
+	if _, err := c.doRequest(sendMessageMethod, q); err != nil {
+		return e.Wrap("can't send message", err)
+	}
+
+	return nil
+}
+
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	defer func() {
+		err = e.WrapIfErr("can't do request", err)
+	}()
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
--- main.go
+++ main.go
@@ -1,13 +1,48 @@
 package main
 
-func main() {
-	// token = flags.Get(token)
+import (
+	"flag"
+	"github.com/dmitry1721/adviser-bot.git/clients/telegram"
+	"log"
+)
 
-	// thClient = telegram.New(token)
+func main() {
+
+	tgClient = telegram.New(mustHost(), mustToken())
 
 	// fetcher = fetcher.New()
 
 	// processor = processor.New()
 
 	// consumer = consumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	token := flag.String(
+		"tg-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token id not specified")
+	}
+
+	return *token
+}
+
+func mustHost() string {
+	host := flag.String(
+		"tg-bot-host",
+		"",
+		"telegram bot host",
+	)
+	flag.Parse()
+
+	if *host == "" {
+		log.Fatal("host not specified")
+	}
+
+	return *host
 }
